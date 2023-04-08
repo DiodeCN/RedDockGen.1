2a95dfb9f4a890d64a2112bff2452a2b262680a2
DiodeCN <1069523953@qq.com>
2023-04-09 06:13:17 +0800

    删掉那么多烦人的东西

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,23 +1,3 @@
-import { useState } from 'react';
-
-// material-ui
-import {
-    Avatar,
-    AvatarGroup,
-    Box,
-    Button,
-    Grid,
-    List,
-    ListItemAvatar,
-    ListItemButton,
-    ListItemSecondaryAction,
-    ListItemText,
-    MenuItem,
-    Stack,
-    TextField,
-    Typography
-} from '@mui/material';
-
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
@@ -25,7 +5,7 @@ const DashboardDefault = () => {
     return (
         <>
 
-        
+
         </>
     );
 };
