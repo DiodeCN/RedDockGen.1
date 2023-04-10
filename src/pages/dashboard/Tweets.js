e17feb4f27c42c5374ff7472f550e1afe82f7ac0
DiodeCN <1069523953@qq.com>
2023-04-11 05:17:58 +0800

    转移

--- src/pages/dashboard/Tweets.js
+++ src/pages/dashboard/Tweets.js
@@ -1,85 +1,68 @@
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { Box, Card, CardContent, CardActions, IconButton, Typography, Avatar } from '@mui/material';
+import { ThumbUp, Star, Repeat } from '@mui/icons-material';
+import axios from 'axios';
 
-// material-ui
-import { useTheme } from '@mui/material/styles';
-
-// third-party
-import ReactApexChart from 'react-apexcharts';
-
-// chart options
-const barChartOptions = {
-    chart: {
-        type: 'bar',
-        height: 365,
-        toolbar: {
-            show: false
-        }
-    },
-    plotOptions: {
-        bar: {
-            columnWidth: '45%',
-            borderRadius: 4
-        }
-    },
-    dataLabels: {
-        enabled: false
-    },
-    xaxis: {
-        categories: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-        axisBorder: {
-            show: false
-        },
-        axisTicks: {
-            show: false
-        }
-    },
-    yaxis: {
-        show: false
-    },
-    grid: {
-        show: false
-    }
-};
-
-// ==============================|| MONTHLY BAR CHART ||============================== //
-
-const MonthlyBarChart = () => {
-    const theme = useTheme();
-
-    const { primary, secondary } = theme.palette.text;
-    const info = theme.palette.info.light;
-
-    const [series] = useState([
-        {
-            data: [80, 95, 70, 42, 65, 55, 78]
-        }
-    ]);
-
-    const [options, setOptions] = useState(barChartOptions);
+const Tweets = ()  => {
+    const [tweets, setTweets] = useState([]);
 
     useEffect(() => {
-        setOptions((prevState) => ({
-            ...prevState,
-            colors: [info],
-            xaxis: {
-                labels: {
-                    style: {
-                        colors: [secondary, secondary, secondary, secondary, secondary, secondary, secondary]
-                    }
-                }
-            },
-            tooltip: {
-                theme: 'light'
-            }
-        }));
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [primary, info, secondary]);
-
-    return (
-        <div id="chart">
-            <ReactApexChart options={options} series={series} type="bar" height={365} />
-        </div>
-    );
-};
-
-export default MonthlyBarChart;
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('http://192.168.1.6:8080/api/tweets');
+            setTweets(response.data);
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        fetchData();
+      }, []);
+      
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', p: 4 }}>
+    {tweets.map(({ id, avatar_url, name, content, hours_since_post, likes, favorites, retweets }) => (
+      <Card key={id} sx={{ p: 2, borderRadius: '12px', boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)', width: '100%' }}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 1 }}>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
+            <Avatar src={avatar_url} alt={`${name}'s avatar`} sx={{mr: 1 }}/>
+            <Typography component="div" variant="subtitle2" sx={{ color: 'primary.main', fontSize: '1.5rem' }}>
+              {name}
+            </Typography>
+            <Typography component="div" variant="caption" sx={{ color: 'secondary.main', fontSize: '1.12rem'}}>
+              @{id}
+            </Typography>
+            <Typography component="div" variant="caption" sx={{ color: '#999', fontSize: '1.12rem'}}>
+              {hours_since_post}h
+            </Typography>
+          </Box>
+        </Box>
+        <CardContent>
+          <Typography variant="body1" sx={{ fontSize: '1.2rem' }}>{content}</Typography>
+          </CardContent>
+    <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
+        <IconButton aria-label="like" sx={{ flexGrow: 1 }}>
+          <ThumbUp />
+          <Typography variant="body2" sx={{ fontSize: '1.2rem' }}>
+            {likes}
+          </Typography>
+        </IconButton>
+        <IconButton aria-label="favorite" sx={{ flexGrow: 1 }}>
+          <Star />
+          <Typography variant="body2" sx={{ fontSize: '1.2rem' }}>
+            {favorites}
+          </Typography>
+        </IconButton>
+        <IconButton aria-label="retweet" sx={{ flexGrow: 1 }}>
+          <Repeat />
+          <Typography variant="body2" sx={{ fontSize: '1.2rem' }}>
+            {retweets}
+          </Typography>
+        </IconButton>
+      </Box>
+    </CardActions>
+  </Card>
+))}
+</Box>
+}
+export default Tweets;
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,70 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Box, Card, CardContent, CardActions, IconButton, Typography, Avatar } from '@mui/material';
-import { ThumbUp, Star, Repeat } from '@mui/icons-material';
 
 const DashboardDefault = () => {
   const [tweets, setTweets] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://192.168.1.6:8080/api/tweets');
-        setTweets(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
 
-    fetchData();
-  }, []);
 
   return (
-    <Box sx={{ display: 'flex', flexDirection: 'column', gap: '1rem', p: 4 }}>
-      {tweets.map(({ id, avatar_url, name, content, hours_since_post, likes, favorites, retweets }) => (
-        <Card key={id} sx={{ p: 2, borderRadius: '12px', boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)', width: '100%' }}>
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 1 }}>
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}>
-              <Avatar src={avatar_url} alt={`${name}'s avatar`} sx={{mr: 1 }}/>
-              <Typography component="div" variant="subtitle2" sx={{ color: 'primary.main', fontSize: '1.5rem' }}>
-                {name}
-              </Typography>
-              <Typography component="div" variant="caption" sx={{ color: 'secondary.main', fontSize: '1.12rem'}}>
-                @{id}
-              </Typography>
-              <Typography component="div" variant="caption" sx={{ color: '#999', fontSize: '1.12rem'}}>
-                {hours_since_post}h
-              </Typography>
-            </Box>
-          </Box>
-          <CardContent>
-            <Typography variant="body1" sx={{ fontSize: '1.2rem' }}>{content}</Typography>
-            </CardContent>
-      <CardActions sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%' }}>
-          <IconButton aria-label="like" sx={{ flexGrow: 1 }}>
-            <ThumbUp />
-            <Typography variant="body2" sx={{ fontSize: '1.2rem' }}>
-              {likes}
-            </Typography>
-          </IconButton>
-          <IconButton aria-label="favorite" sx={{ flexGrow: 1 }}>
-            <Star />
-            <Typography variant="body2" sx={{ fontSize: '1.2rem' }}>
-              {favorites}
-            </Typography>
-          </IconButton>
-          <IconButton aria-label="retweet" sx={{ flexGrow: 1 }}>
-            <Repeat />
-            <Typography variant="body2" sx={{ fontSize: '1.2rem' }}>
-              {retweets}
-            </Typography>
-          </IconButton>
-        </Box>
-      </CardActions>
-    </Card>
-  ))}
-</Box>
+
 
 
   );
