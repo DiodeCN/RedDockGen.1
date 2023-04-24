c396f3e990da97a3f634972be3095ba0514f888f
DiodeCN <1069523953@qq.com>
2023-04-25 04:38:18 +0800

    更好的发布页

--- src/pages/moneycenter/ReleasePage.js
+++ src/pages/moneycenter/ReleasePage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   AppBar,
   Box,
@@ -11,9 +11,10 @@ import {
   TextField,
   Toolbar,
   Typography,
-} from '@mui/material';
-import ReactMarkdown from 'react-markdown';
-import gfm from 'remark-gfm'; 
+  InputLabel
+} from "@mui/material";
+import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 const insertTextAtEnd = (textArea, newText) => {
   const { value } = textArea;
@@ -21,8 +22,8 @@ const insertTextAtEnd = (textArea, newText) => {
 };
 
 const ReleasePage = () => {
-  const [markdownText, setMarkdownText] = useState('');
-  const [articleType, setArticleType] = useState('分享');
+  const [markdownText, setMarkdownText] = useState("");
+  const [articleType, setArticleType] = useState("分享");
 
   const textAreaRef = React.createRef();
 
@@ -49,12 +50,12 @@ const ReleasePage = () => {
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        gap: '1rem',
-        width: '100%',
-        maxWidth: '800px',
-        margin: '0 auto',
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        width: "100%",
+        maxWidth: "800px",
+        margin: "0 auto"
       }}
     >
       <AppBar position="static" color="transparent" elevation={0}>
@@ -62,14 +63,28 @@ const ReleasePage = () => {
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             文本编辑
           </Typography>
-          <Button onClick={() => handleInsertClick('# 芝士H1 标题\n\n')}>H1</Button>
-          <Button onClick={() => handleInsertClick('## 芝士H2 标题\n\n')}>H2</Button>
-          <Button onClick={() => handleInsertClick('### 芝士H3 标题\n\n')}>H3</Button>
-          <Button onClick={() => handleInsertClick('**芝士粗体**')}>粗体</Button>
-          <Button onClick={() => handleInsertClick('_芝士斜体_')}>斜体</Button>
-          <Button onClick={() => handleInsertClick('~~芝士删除线~~')}>删除线</Button>
-          <Button onClick={() => handleInsertClick('\n\n- 芝士列表项')}>列表项</Button>
-          <Button onClick={() => handleInsertClick('\n\n1. 芝士有序列表项')}>有序列表项</Button>
+          <Button onClick={() => handleInsertClick("# 芝士H1 标题\n\n")}>
+            H1
+          </Button>
+          <Button onClick={() => handleInsertClick("## 芝士H2 标题\n\n")}>
+            H2
+          </Button>
+          <Button onClick={() => handleInsertClick("### 芝士H3 标题\n\n")}>
+            H3
+          </Button>
+          <Button onClick={() => handleInsertClick("**芝士粗体**")}>
+            粗体
+          </Button>
+          <Button onClick={() => handleInsertClick("_芝士斜体_")}>斜体</Button>
+          <Button onClick={() => handleInsertClick("~~芝士删除线~~")}>
+            删除线
+          </Button>
+          <Button onClick={() => handleInsertClick("\n\n- 芝士列表项")}>
+            列表项
+          </Button>
+          <Button onClick={() => handleInsertClick("\n\n1. 芝士有序列表项")}>
+            有序列表项
+          </Button>
         </Toolbar>
       </AppBar>
       <TextField
@@ -79,15 +94,15 @@ const ReleasePage = () => {
         value={markdownText}
         onChange={handleTextChange}
         fullWidth
-        sx={{ borderRadius: '12px' }}
+        sx={{ borderRadius: "12px" }}
         inputRef={textAreaRef}
       />
 
       <Card
         sx={{
-          borderRadius: '12px',
-          boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)',
-          width: '100%',
+          borderRadius: "12px",
+          boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",
+          width: "100%"
         }}
       >
         <CardContent>
@@ -95,25 +110,32 @@ const ReleasePage = () => {
         </CardContent>
       </Card>
 
-      <FormControl fullWidth sx={{marginTop: '1rem'}}>
+      <FormControl fullWidth sx={{ marginTop: "1rem" }}>
+        <InputLabel id="article-type-label">文章类型</InputLabel>
         <Select
-          labelId="article-type"
+          labelId="article-type-label"
           id="article-type-select"
           value={articleType}
           onChange={handleArticleTypeChange}
-          label="文章类型"
-          >
+        >
           <MenuItem value="分享">分享</MenuItem>
           <MenuItem value="请求">请求</MenuItem>
           <MenuItem value="闲聊">闲聊</MenuItem>
           <MenuItem value="日志">日志</MenuItem>
-          </Select>
-          </FormControl>
-          <Button fullWidth variant="contained" color="primary" onClick={handleSubmit} sx={{marginTop: '1rem'}}>
-    发送
-  </Button>
-</Box>
-);
+        </Select>
+      </FormControl>
+
+      <Button
+        fullWidth
+        variant="contained"
+        color="primary"
+        onClick={handleSubmit}
+        sx={{ marginTop: "1rem" }}
+      >
+        发送
+      </Button>
+    </Box>
+  );
 };
 
 export default ReleasePage;
