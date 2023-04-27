13f575a5708e78621f11424706c53eb0dd87badf
DiodeCN <1069523953@qq.com>
2023-04-27 21:48:16 +0800

    uid

--- src/layout/MainLayout/Header/HeaderContent/Profile/index.js
+++ src/layout/MainLayout/Header/HeaderContent/Profile/index.js
@@ -56,7 +56,7 @@ function a11yProps(index) {
 
 const Profile = () => {
     const theme = useTheme();
-    const userId = sessionStorage.getItem('userId');
+    const userId = sessionStorage.getItem('uid');
     const [token, setToken] = useState(sessionStorage.getItem('token') || localStorage.getItem('token'));
     const [avatarUrl, setAvatarUrl] = useState(null);
     const [isTokenReady, setIsTokenReady] = useState(false);
