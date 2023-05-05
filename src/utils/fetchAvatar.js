4dcacf396a5731428dfef23e4b00e3c79de0e256
DiodeCN <1069523953@qq.com>
2023-05-05 17:07:44 +0800

    啊啊啊从

--- src/utils/fetchAvatar.js
+++ src/utils/fetchAvatar.js
@@ -1,24 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import fetchAvatar from './fetchAvatar';
+import axios from 'axios';
 
-// Your component
-const YourComponent = (props) => {
-  const [avatarUrl, setAvatarUrl] = useState(null);
-  const userId = props.userId; // Replace this line with the appropriate user ID retrieval method
+const fetchAvatar = async (userId) => {
+  const token = sessionStorage.getItem('token') || localStorage.getItem('token');
 
-  useEffect(() => {
-    const fetchAndSetAvatar = async () => {
-      const cachedAvatarUrl = sessionStorage.getItem(`avatar_${userId}`);
-      if (cachedAvatarUrl) {
-        setAvatarUrl(cachedAvatarUrl);
-      } else {
-        const fetchedAvatarUrl = await fetchAvatar(userId);
-        setAvatarUrl(fetchedAvatarUrl);
-      }
-    };
-
-    fetchAndSetAvatar();
-  }, [userId]);
-
-  // Rest of your component
+  try {
+    const response = await axios.post(`https://avatar.cloudepot.cn/api/avatar/${userId}`, {
+      token: token,
+    });
+    const fetchedAvatarUrl = response.request.responseURL;
+    sessionStorage.setItem(`avatar_${userId}`, fetchedAvatarUrl);
+    return fetchedAvatarUrl;
+  } catch (error) {
+    console.log('Error fetching avatar:', error);
+    return null;
+  }
 };
+
+export default fetchAvatar;
